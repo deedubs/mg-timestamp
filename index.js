47582c1bc9df5a52f2d25291aed513f204d7b477
Dan Williams <me@deedubs.com>
2013-05-19 10:25:38 -0400

    first pass

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = function (schema) {
+  schema.add({
+    created_at: { type: Date, 'default': Date.now },
+    updated_at: { type: Date }
+  });
+
+  schema.pre('save', function (next) {
+    this.updated_at = new Date();  
+
+    next();
+  });
+};
